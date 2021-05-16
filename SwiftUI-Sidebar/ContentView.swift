3234ece68b6d230127afdee40f3e5e125aa82f9e
Philipp <pd95@users.noreply.github.com>
2021-05-16 13:23:54 +0200

    Initial Commit

---
+++ SwiftUI-Sidebar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Sidebar
+//
+//  Created by Philipp on 16.05.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Sidebar/SwiftUI_SidebarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SidebarApp.swift
+//  SwiftUI-Sidebar
+//
+//  Created by Philipp on 16.05.21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SidebarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
